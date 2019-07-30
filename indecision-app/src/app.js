9cecdaf6015db2c0475404964d51c0d90f866783
ds2113 <ds2113@att.com>
2019-07-30 15:41:25 -0500

    completed up to (and including) video 20

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -21,7 +21,13 @@ const onFormSubmit = (e) => {
 const removeAll = () => {
     app.options = [];
     render();
-}
+};
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
 
 const appRoot = document.querySelector('#app');
 
@@ -31,11 +37,12 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={removeAll}>Remove All</button>
             <ol>
-                <li>Item one</li>
-                <li>Item two</li>
+                {
+                    app.options.map((option) => <li key="{option}">{option}</li>)
+                    }
             </ol>
             <form onSubmit={onFormSubmit}>
                 <input type="text" name="option"/>
