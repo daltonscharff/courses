828862ef18fbadb742c45a441beb7e35470b30d5
Scharff, Dalton (ds2113) <ds2113@att.com>
2019-07-31 11:36:47 -0500

    complete through video 38

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,31 +1,33 @@
-const app = {
-    header: "Visibility Toggle",
-    buttonText: ["Show details", "Hide details"],
-    isVisible: false
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        };
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>VisibilityToggle</h1>
+                <button onClick={this.handleToggleVisibility}>
+                    {this.state.visibility ? "Hide details" : "Show details"}
+                </button>
+                {this.state.visibility && (
+                    <p>
+                        Hey. There are some details you can now see!
+                    </p>
+                )}
+            </div>
+        );
+    }
 }
 
-const changeState = () => {
-    app.isVisible = !app.isVisible;
-    render();
-};
-
-const appRoot = document.querySelector('#app');
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>{app.header}</h1>
-            <button onClick={changeState}>
-                {app.isVisible ? "Hide details" : "Show details"}
-            </button>
-            {app.isVisible && (
-            <p>
-                Hey. There are some details you can now see!
-            </p>
-            )}
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
-
-render();
+ReactDOM.render(<VisibilityToggle />, document.querySelector('#app'));
