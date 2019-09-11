0b064c280f9836e64ac9337ebd99474a8e0b16d2
Scharff, Dalton (ds2113) <ds2113@att.com>
2019-09-11 15:21:09 -0500

    complete through video 140

--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -53,10 +53,10 @@ export class ExpenseListFilters extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-        filters: state.filters
+    filters: state.filters
 });
 
-const mapDispatchToProps = () => ({
+const mapDispatchToProps = (dispatch) => ({
     setTextFilter: (text) => dispatch(setTextFilter(text)),
     sortByDate: () => dispatch(sortByDate()),
     sortByAmount: () => dispatch(sortByAmount()),
