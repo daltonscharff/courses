b41c27b43d141534f74e3ac3dd89296553ddb3c0
Dalton Scharff (ds2113) <ds2113@att.com>
2019-09-12 13:15:43 -0500

    complete though 151

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -14,24 +14,70 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-const onValueChange = database.ref().on('value', (snapshot) => {
-    const val = snapshot.val();
-    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// child_removed
+database.ref('expenses').on('child_removed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val())
 });
+
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val())
+});
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
+    console.log(snapshot.key, snapshot.val())
+});
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+//         console.log(expenses);
+// });
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+//         console.log(expenses);
+//     });
+
+// database.ref('notes/-LoaX6YDgaMwWf2d41JG').update({
+//     body: 'Buy food'
+// })
+
+// database.ref('notes').push({
+//     title: "course topics",
+//     body: "React Native, Angular, Python"
+// });
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// });
 
 // const onValueChange = database.ref().on('value', (snapshot) => {
 //     console.log(snapshot.val());
 // });
 
-setTimeout(() => {
-    database.ref('age').set(23);
-}, 3500);
+// setTimeout(() => {
+//     database.ref('age').set(23);
+// }, 3500);
 
-setTimeout(() => {
-    database.ref('job/company').set('Steam');
-}, 7000);
-
-
+// setTimeout(() => {
+//     database.ref('job/company').set('Steam');
+// }, 7000);
 
 // database.ref('location/city').once('value').then((snapshot) => {
 //     const val = snapshot.val();
