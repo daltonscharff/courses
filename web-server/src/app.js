51d26bbe77938a1de4b378219f212f3555c79bc9
Scharff, Dalton (ds2113) <ds2113@att.com>
2019-09-19 11:34:49 -0500

    complete through video 67

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
@@ -86,6 +87,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000');
 })
