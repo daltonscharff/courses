da667987b4fccc1ea51022366597631725c48681
Dalton Scharff <dalton.scharff@gmail.com>
2019-09-20 20:39:16 -0500

    complete through video 78

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,19 +1,33 @@
-// CRUD
+const config = require('./config');
+const { MongoClient, ObjectID } = require('mongodb');
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
-
-const connectionURL = 'mongodb://127.0.0.1:27017';
+const connectionURL = `mongodb+srv://${config.mongodb.username}:${config.mongodb.password}@cluster0-qpyfg.mongodb.net/test?retryWrites=true&w=majority`;
 const databaseName = 'task-manager';
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to database');
-    }
+    if (error) return console.log('Unable to connect to database');
 
     const db = client.db(databaseName);
-    db.collection('users').insertOne({
-        name: 'Dalton',
-        age: 23
+
+    // db.collection('users').findOne({
+    //     _id: new ObjectID('5d857770776d632fe87f0458')
+    // },(error, user) => {
+    //     if (error) return console.log('Unable to fetch');
+
+    //     console.log(user);
+    // });
+
+    // db.collection('users').find({ name: "Dalton" }).toArray((error, users) => {
+    //     console.log(users);
+    // });
+
+    db.collection('tasks').findOne({
+        _id: new ObjectID('5d85780441d11b196c712f7d')
+    }, (error, task) => {
+        console.log(task);
     });
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks);
+    })
 });
