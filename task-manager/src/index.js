c8778bf7787519275ef814871214b2f0960db1ff
Dalton Scharff <dalton.scharff@gmail.com>
2019-09-21 09:58:34 -0500

    complete through vieo 92

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -17,12 +17,46 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id).then((users) => {
+        if (!users) return res.status(404).send();
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+});
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
     task.save().then(() => {
         res.status(201).send(task);
     }).catch((error) => {
         res.status(400).send(error);
+    });
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+});
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((tasks) => {
+        if (!tasks) return res.status(404).send();
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error);
     });
 });
 
