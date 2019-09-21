e0cd5f2ec370d9d23b7015fee2bd79ab7d58e5f9
Dalton Scharff <dalton.scharff@gmail.com>
2019-09-21 09:22:49 -0500

    complete through video 90

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 const config = require('../../config');
 
 const databaseName = 'task-manager-api';
@@ -9,79 +8,3 @@ mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useCreateIndex: true
 });
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
-    }
-});
-
-const me = new User({
-    name: '   Dalton    ',
-    email: 'dalton.SCHARFF@gmail.com',
-    password: '       phone098     '
-});
-
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log('error:', error);
-// });
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task = new Task({
-    description: ' Eat lunch   '
-});
-
-task.save().then((result) => {
-    console.log(result);
-}).catch((error) => {
-    console.log(error);
-})
-
-
-
