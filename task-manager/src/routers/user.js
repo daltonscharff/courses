c987a0b6aa14269a7f4a0334d23ad4d32b711134
Dalton Scharff <dalton.scharff@gmail.com>
2019-09-22 15:49:51 -0500

    complete through video 125

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
@@ -73,6 +74,22 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send(e);
     }
+});
+
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 10000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) return cb(new Error('Please upload a PDF'));
+
+        cb(undefined, true);
+    }
+});
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send();
 });
 
 module.exports = router;
