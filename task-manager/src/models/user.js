cd599122db77c4dfc5236aaf61a46721f6a3a55c
Dalton Scharff <dalton.scharff@gmail.com>
2019-09-22 10:37:26 -0500

    complete through video 116

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../../config');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -50,6 +51,22 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+});
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    delete userObject.password;
+    delete userObject.tokens;
+
+    return userObject;
+};
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
     const token = jwt.sign({ _id: user.id.toString() }, config.jwt.secret);
@@ -78,6 +95,12 @@ userSchema.pre('save', async function (next) {
         user.password = await bcrypt.hash(user.password, 8);
     }
 
+    next();
+});
+
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id });
     next();
 });
 
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -22,48 +22,54 @@ router.post('/users/login', async (req, res) => {
     } catch (e) {
         res.status(400).send();
     }
-})
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
+        await req.user.save();
+
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save();
+
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
 
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-    try {
-        const users = await User.findById(req.params.id);
-        if (!users) return res.status(404).send();
-        res.send(users);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
     if (!isValidOperation) return res.status(404).send({ error: 'Invalid updates' });
 
     try {
-
-        const user = await User.findById(req.params.id);
-        updates.forEach((update) => user[update] = req.body[update]
+        updates.forEach((update) => req.user[update] = req.body[update]
         );
-        await user.save();
-
-        if (!user) return res.status(404).send();
-        res.send(user);
+        await req.user.save();
+        res.send(req.user);
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id);
-
-        if (!user) return res.status(404).send();
-        res.send(user);
+        await req.user.remove();
+        res.send(req.user);
     } catch (e) {
         res.status(500).send(e);
     }
