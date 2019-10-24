d277f70486de701a528ceb23a37087c3ffc67cee
Dalton Scharff (ds2113) <ds2113@att.com>
2019-10-24 12:32:03 -0500

    complete through video 200

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -1,25 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import NoteApp from './components/NoteApp';
 
-const App = (props) => {
-    const [count, setCount] = useState(props.count);
-
-    return (
-        <div>
-            <p>The current count is {count}</p>
-            <button onClick={() => setCount(count - 1)}>-1</button>
-            <button onClick={() => setCount(props.count)}>Reset</button>
-            <button onClick={() => setCount(count + 1)}>+1</button>
-        </div>
-    )
-};
-
-App.defaultProps = {
-    count: 0
-};
-
-ReactDOM.render(<App count={2}/>, document.getElementById('root'));
+ReactDOM.render(<NoteApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
