34b0a2a307d1b15479207904dd42a431bfa12d0f
Dalton Scharff <dalton.scharff@gmail.com>
2022-03-20 13:34:33 -0500

    completes mock service worker section

---
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import { useEffect, useState } from "react";
+import { ScoopOption } from "./ScoopOption";
+import { ToppingOption } from "./ToppingOption";
+import { AlertBanner } from "../common/AlertBanner";
+
+export const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((_) => setError(true));
+  }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
